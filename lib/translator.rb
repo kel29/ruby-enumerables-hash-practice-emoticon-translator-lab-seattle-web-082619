ae34cb831f5c49ea457fb756482681562de13d73
Krista <krista.loven.87@gmail.com>
2019-08-21 16:28:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,11 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)["get_emoticon"][emoticon]
-  puts "Sorry, that emoticon was not found" if load_library(file_path)["get_emoticon"][emoticon] == nil 
+  if load_library(file_path)["get_emoticon"][emoticon]
+    load_library(file_path)["get_emoticon"][emoticon]
+  else
+    puts "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning(file_path, emoticon)

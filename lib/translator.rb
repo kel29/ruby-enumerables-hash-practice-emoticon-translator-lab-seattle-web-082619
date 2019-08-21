252b0d8c745e65e7371fb033af04248e57e73032
Krista <krista.loven.87@gmail.com>
2019-08-21 16:22:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)["get_emoticon"][emoticon] ? load_library(file_path)["get_emoticon"][emoticon] : puts "Sorry, that is not a known emoticon."
+  return load_library(file_path)["get_emoticon"][emoticon] ? load_library(file_path)["get_emoticon"][emoticon] : "Sorry, that is not a known emoticon."
 
 end
 

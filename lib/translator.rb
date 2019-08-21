65d8b429496a2dff14895a3696ece149a45891a1
Krista <krista.loven.87@gmail.com>
2019-08-21 16:15:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path).each do |e|
+  load_library(file_path).each do |t, e|
     puts e["get_emoticon"][emoticon]
   end
 end
